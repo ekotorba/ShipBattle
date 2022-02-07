30aeeadfd26a3623e3fdc873a7605f500be5286b
ekotorba <falkowskipiotr@outlook.com>
2022-02-07 18:39:46 +0100

    Centralizing Configuration

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,10 @@ require_once __DIR__. '/lib/BattleManager.php';
 require_once __DIR__. '/lib/ShipLoader.php';
 require_once __DIR__. '/lib/BattleResult.php';
 
-
+$configuration =[
+    'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => ''
+];
 
 
--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,6 +4,18 @@ class ShipLoader
 {
 
     private $pdo;
+    private $dbDsn;
+    private $dbUser;
+    private $dbPass;
+
+    public function __construct($dbDsn, $dbUser, $dbPass){
+
+        $this->dbDsn = $dbDsn;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+
+    }
+
     /**
      * @return Ship[]
      */
@@ -66,7 +78,7 @@ class ShipLoader
     {
 
         if($this->pdo === null){
-            $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+            $pdo = new PDO($this->dbDsn, $this->dbUser, $this->dbPass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $this->pdo = $pdo;
